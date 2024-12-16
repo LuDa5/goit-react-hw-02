7500977aae1c3c622e53128144922a9998d21ab0
LuDa5 <133536632+LuDa5@users.noreply.github.com>
2024-12-16 16:18:30 +0200

    hv2

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Feedback.module.css";
+
+const Feedback = () => {
+  return (
+    <div className={styles.feedback}>
+      <h2>Feedback</h2>
+    </div>
+  );
+};
+
+export default Feedback;
---
+++ src/components/Options.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Options.module.css";
+
+const Options = ({ onLeaveFeedback, totalFeedback, onReset }) => {
+  return (
+    <div className={styles.options}>
+      <button onClick={() => onLeaveFeedback("good")}>Good</button>
+      <button onClick={() => onLeaveFeedback("neutral")}>Neutral</button>
+      <button onClick={() => onLeaveFeedback("bad")}>Bad</button>
+      {totalFeedback > 0 && (
+        <button className={styles.reset} onClick={onReset}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Options;
